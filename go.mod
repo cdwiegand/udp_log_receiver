a6840fed95830ca007056276c2ced25e01327408
Chris Wiegand <chris@wiegandtech.net>
2022-11-17 20:36:28 -0700

    first version, works from a .net nlog network target using udp://127.0.0.1:10000, displays count of lines in memory and the line itself for debugging right now

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cdwiegand/udp_log_receiver
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"log"
+	"net"
+)
+
+// thanks to https://www.golinuxcloud.com/golang-udp-server-client/ for the basic UDP listener code!
+var logs = list.New()
+
+func main() {
+	udpServer, err := net.ListenPacket("udp", ":10000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer udpServer.Close()
+
+	for {
+		buf := make([]byte, 65000)
+		_, _, err := udpServer.ReadFrom(buf)
+		if err != nil {
+			continue
+		}
+		line := string(buf)
+		if logs.Len() >= 5000 {
+			logs.Remove(logs.Back())
+		}
+		logs.PushFront(line)
+		newline := fmt.Sprintf("mem: %d: %s", logs.Len(), line)
+		log.Println(newline)
+	}
+}

b887255cbbd0ae2f5c90d782cef2edc8d67ebabd
Chris Wiegand <chris@wiegandtech.net>
2022-11-17 21:28:35 -0700

    try adding initial HTTP server

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"container/list"
 	"log"
 	"net"
+	"net/http"
+	"strings"
+	"time"
 )
 
 // thanks to https://www.golinuxcloud.com/golang-udp-server-client/ for the basic UDP listener code!
 var logs = list.New()
 
 func main() {
+	go runHttpServer() // run in background
+	go runUdpServer()
+	for {
+		time.Sleep(time.Minute)
+	}
+}
+
+func runUdpServer() {
 	udpServer, err := net.ListenPacket("udp", ":10000")
 	if err != nil {
 		log.Fatal(err)
+		panic(err)
 	}
 	defer udpServer.Close()
 
@@ -27,7 +39,39 @@ func main() {
 			logs.Remove(logs.Back())
 		}
 		logs.PushFront(line)
-		//newline := fmt.Sprintf("mem: %d: %s", logs.Len(), line)
-		//log.Println(newline)
 	}
+}
+
+func runHttpServer() {
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      handleHTTPHandler(handleHTTPRequest),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	srv.SetKeepAlivesEnabled(true)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+}
+
+func handleHTTPHandler(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f(w, r)
+	}
+}
+
+func handleHTTPRequest(w http.ResponseWriter, r *http.Request) {
+	values := []string{}
+
+	for temp := logs.Front(); temp != nil; temp = temp.Next() {
+		values = append(values, temp.Value.(string))
+	}
+	body := strings.Join(values, "\n")
+
+	w.Header().Set("X-Hello", "Darkness, my old friend")
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(200)
+	w.Write([]byte(body))
 }
